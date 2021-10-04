c606114d259365bbe209b761c6aeef020ea74d66
BRUNO RIBEIRO DE OLIVEIRA <21193381@CFP127ALUNO.LOCAL>
2021-10-04 16:56:33 -0300

    dashboard feita

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.imcapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar!!.hide()
+
+        val buttonEntrar = findViewById<Button>(R.id.button_entrar)
+
+        buttonEntrar.setOnClickListener{
+            val abrirDashboard = Intent (this, DashboardActivity::class.java)
+            startActivity(abrirDashboard)
+        }
     }
 }
