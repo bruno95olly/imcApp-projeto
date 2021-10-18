83123a863dd44efe27d8ab3e02a71388b69e7801
BRUNO RIBEIRO DE OLIVEIRA <21193381@CFP127ALUNO.LOCAL>
2021-10-18 16:52:21 -0300

    activity cadastro

---
+++ app/src/main/java/com/example/imcapp/activity_novo_usuario.kt
@@ -0,0 +1,11 @@
+package com.example.imcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_novo_usuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo_usuario)
+    }
+}
