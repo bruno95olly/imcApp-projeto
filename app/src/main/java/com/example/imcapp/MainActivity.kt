171eaffd6f504c3f1f9fa568839188d9bea0917f
BRUNO RIBEIRO DE OLIVEIRA <21193381@CFP127ALUNO.LOCAL>
2021-10-25 16:59:37 -0300

    cadastro realizado

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.imcapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.hide()
 
         val buttonEntrar = findViewById<Button>(R.id.button_entrar)
+        val btnCadastrar = findViewById<Button>(R.id.button_cadastrar)
 
         buttonEntrar.setOnClickListener{
             val abrirDashboard = Intent (this, DashboardActivity::class.java)
             startActivity(abrirDashboard)
         }
+        btnCadastrar.setOnClickListener{
+            val abrirCadastro = Intent(this, activity_novo_usuario::class.java)
+            startActivity(abrirCadastro)
+        }
     }
+
+
 }
--- app/src/main/java/com/example/imcapp/activity_novo_usuario.kt
+++ app/src/main/java/com/example/imcapp/activity_novo_usuario.kt
@@ -2,10 +2,68 @@ package com.example.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.RadioButton
 
 class activity_novo_usuario : AppCompatActivity() {
+
+    lateinit var editNome: EditText
+    lateinit var editProfissao: EditText
+    lateinit var editAltura: EditText
+    lateinit var editNascimento: EditText
+    lateinit var editMasculino: RadioButton
+    lateinit var editFeminino: RadioButton
+    lateinit var editEmail: EditText
+    lateinit var editSenha: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_novo_usuario)
+
+        editEmail = findViewById(R.id.edit_email)
+        editSenha = findViewById(R.id.edit_senha)
+        editNome = findViewById(R.id.edit_nome)
+        editProfissao = findViewById(R.id.edit_profissao)
+        editAltura = findViewById(R.id.edit_altura)
+        editNascimento = findViewById(R.id.edit_nascimento)
+        editMasculino = findViewById(R.id.rdo_masculino)
+        editFeminino = findViewById(R.id.rdo_feminino)
+
+        supportActionBar!!.title = "Nova Conta"
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_cadastro, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(validarCampos()){
+
+        }
+        else{
+
+        }
+
+        return true
+    }
+
+    private fun validarCampos() : Boolean{
+        var valido = true
+
+        if(editEmail.text.isEmpty()){
+           editEmail.error = "Email é obrigatório!"
+            valido = false
+        }
+
+        if(editSenha.text.isEmpty()){
+            editSenha.error = "Senha é obrigatório"
+            valido = false
+        }
+
+        return valido
+    }
+
 }
